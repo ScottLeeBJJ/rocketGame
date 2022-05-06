de2e62a406c3d4d8edeb4ed458fdb867de4432e8
ScottLeeBJJ <scottlee5455@outlook.com>
2022-05-05 20:13:47 -0700

    slight update to game details

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -15,6 +15,8 @@ public class Movement : MonoBehaviour
     Rigidbody rb;
     AudioSource audioSource;
 
+    //testing update
+
     // Start is called before the first frame update
     void Start()
     {
