b769e15965e3d335fb6dbd08019b520caffefc31
ScottLeeBJJ <scottlee5455@outlook.com>
2022-05-08 23:40:03 -0700

    protected against NaN error

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -18,6 +18,7 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (period == 0) { return; }
         float cycles = Time.time / period;
 
         const float tau = Mathf.PI * 2;
