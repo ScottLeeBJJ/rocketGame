6753a66e379850a42501eb61355d6f6d719380d9
ScottLeeBJJ <scottlee5455@outlook.com>
2022-05-08 23:58:41 -0700

    updated quit application

--- Assets/Scripts/QuitApplication.cs
+++ Assets/Scripts/QuitApplication.cs
@@ -4,19 +4,12 @@ using UnityEngine;
 
 public class QuitApplication : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            Application.Quit();
             Debug.Log("Quitting Application");
+            Application.Quit();
         }
     }
 }
